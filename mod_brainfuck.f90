97d077aa404ceb85ffddd4a555434ad4d94e0cd0
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2009-01-18 01:28:37 +0100

    First attempt at looping support
    This appears to be broken, most likely due to the jump offsets being wrong.
    I'll look into this tomorrow.

--- mod_brainfuck.f90
+++ mod_brainfuck.f90
@@ -5,6 +5,36 @@ module mod_brainfuck
   integer :: ptr, ip
   
 contains
+  integer pure function next(haystack, needle, start, backwards)
+    character(len=*), intent(in) :: haystack
+    character, intent(in) :: needle
+    integer, intent(in) :: start
+    logical, optional, intent(in) :: backwards
+    logical :: bwards
+    integer :: i, strt, end, step
+    
+    next = -1
+
+    if(present(backwards)) bwards = backwards
+    
+    if(bwards) then
+       strt = len(haystack)
+       end = start
+       step = -1
+    else
+       strt = start
+       end = len(haystack)
+       step = 1
+    end if
+
+    do i = strt, end, step
+       if(haystack(i:i) == needle) then
+          next = i
+          return
+       end if
+    end do
+  end function next
+      
   subroutine parse(program)
     character(len=*), intent(in) :: program
     character :: cmd, tmp
@@ -30,14 +60,20 @@ contains
              read(*, *) tmp
              data(ptr) = ichar(tmp)
           case('[')
-             write(*, *) 'should loop here.'
+             if(data(ptr) == 0) then
+                ip = next(program, ']', ip + 1)
+             end if
           case(']')
-             write(*, *) 'should loop here.'
+             if(data(ptr) /= 0) then
+                ip = next(program, '[', ip, .true.)
+             end if
           case default
              write(*, *) 'got unknown: ', cmd
        end select
-       
+
        ip = ip + 1
+
+       write(*, *) ip
 
        if(ip > length) exit
     end do
