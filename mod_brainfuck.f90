f7bcf875cea562027ab601f7436334d780198a0c
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2009-01-29 17:49:16 +0100

    Cleanup

--- mod_brainfuck.f90
+++ mod_brainfuck.f90
@@ -4,37 +4,7 @@ module mod_brainfuck
   integer, dimension(1:30000) :: data
   integer :: ptr, ip
   
-contains
-  integer pure function next(haystack, needle, start, backwards)
-    character(len=*), intent(in) :: haystack
-    character, intent(in) :: needle
-    integer, intent(in) :: start
-    logical, optional, intent(in) :: backwards
-    logical :: bwards
-    integer :: i, strt, end, step
-    
-    next = -1
-
-    if(present(backwards)) bwards = backwards
-    
-    if(bwards) then
-       strt = ip
-       end = 1
-       step = -1
-    else
-       strt = start
-       end = len(haystack)
-       step = 1
-    end if
-
-    do i = strt, end, step
-       if(haystack(i:i) == needle) then
-          next = i
-          return
-       end if
-    end do
-  end function next
-      
+contains 
   subroutine parse(program)
     character(len=*), intent(in) :: program
     character :: cmd, tmp
@@ -63,11 +33,9 @@ contains
           case('[')
              if(data(ptr) == 0) then
                 ip = scan(program((ip + 1):length), ']')
-!                ip = next(program, ']', ip + 1)
              end if
           case(']')
              if(data(ptr) /= 0) then
-!                ip = next(program, '[', ip, .true.)
                 ip = scan(program(1:ip), '[', .true.)
              end if
           case default
