ee48a74e7be8359c4f620c4e39240dc44e3ed79a
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2009-02-03 02:31:38 +0100

    Cleaned up codebase
    Restructured the code to look more readable.

--- brainfuck.f90
+++ brainfuck.f90
@@ -1,6 +1,12 @@
 program brainfuck
-
   use mod_brainfuck
+  implicit none
 
-  call parse("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.")
+  character(len=*), parameter :: program = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."
+
+  call init(data_size = 30000, program_string = program)
+
+  call run
+
+  call cleanup
 end program brainfuck
--- mod_brainfuck.f90
+++ mod_brainfuck.f90
@@ -1,53 +1,91 @@
 module mod_brainfuck
   implicit none
 
-  integer, dimension(1:30000) :: data
-  integer :: ptr, ip
+  private
+  public :: init, run, cleanup
+
+  integer, dimension(:), allocatable :: data
+  character, dimension(:), allocatable :: program
+  integer :: data_pointer, instruction_pointer, program_length
   
 contains 
-  subroutine parse(program)
-    character(len=*), intent(in) :: program
-    character :: cmd, tmp
-    integer :: length
+  subroutine init(data_size, program_string)
+    integer, intent(in) :: data_size
+    character(len=*), intent(in) :: program_string
+
+    integer :: i
+
+    program_length = len(program_string)
+
+    allocate(data(1:data_size))
+    allocate(program(1:program_length))
+
+    do i = 1, program_length
+       program(i) = program_string(i:i)
+    end do
 
     data = 0
-    length = len(program)
-    ip = 1
+    data_pointer = 1
+    instruction_pointer = 1
+  end subroutine init
+
+  subroutine cleanup()
+    deallocate(data)
+    deallocate(program)
+  end subroutine cleanup
+
+  subroutine run()
+    character :: tmp
+    integer :: start
+    
     do
-       cmd = program(ip:ip)
-       
-       select case(cmd)
-          case('<')
-             ptr = ptr - 1
-          case('>')
-             ptr = ptr + 1
-          case('+')
-             data(ptr) = data(ptr) + 1
-          case('-')
-             data(ptr) = data(ptr) - 1
-          case('.')
-             write(*, fmt='(A)', advance='no') char(data(ptr))
-          case(',')
-             read(*, *) tmp
-             data(ptr) = ichar(tmp)
-          case('[')
-             if(data(ptr) == 0) then
-                ip = scan(program((ip + 1):length), ']')
-             end if
-          case(']')
-             if(data(ptr) /= 0) then
-                ip = scan(program(1:ip), '[', .true.)
-             end if
-          case default
-             write(*, *) 'got unknown: ', cmd
+       select case(program(instruction_pointer))
+       case('<')
+          data_pointer = data_pointer - 1
+       case('>')
+          data_pointer = data_pointer + 1
+       case('-')
+          data(data_pointer) = data(data_pointer) - 1
+       case('+')
+          data(data_pointer) = data(data_pointer) + 1
+       case(',')
+          read(*, *) tmp
+          data(data_pointer) = ichar(tmp)
+       case('.')
+          write(*, fmt='(A)', advance='no') char(data(data_pointer))
+       case('[')
+          if(data(data_pointer) == 0) then
+             start = instruction_pointer + 1
+             instruction_pointer = first(program(start:program_length), ']')
+          end if
+       case(']')
+          if(data(data_pointer) /= 0) then
+             instruction_pointer = instruction_pointer + 1 &
+                  - first(program(instruction_pointer:1:-1), '[')
+          end if
+       case default
        end select
-
-       ip = ip + 1
-
-       if(ip > length) exit
+       
+       instruction_pointer = instruction_pointer + 1
+       if(instruction_pointer > program_length) exit
     end do
 
     write(*, *)
-  end subroutine parse
+  end subroutine run
 
+  integer pure function first(haystack, needle)
+    character, dimension(:) :: haystack
+    character :: needle
+    intent(in) :: haystack, needle
+
+    integer :: i
+    first = -1
+
+    do i = lbound(haystack, 1), ubound(haystack, 1)
+       if(haystack(i) == needle) then
+          first = i
+          return
+       end if
+    end do
+  end function first
 end module mod_brainfuck
