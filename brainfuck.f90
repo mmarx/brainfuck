c7d0de1aa142918b964d86c8d6af90d8e3b19600
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2014-09-20 03:13:13 +0200

    Fix reading program from file
    Signed-off-by: Maximilian Marx <mmarx@wh2.tu-dresden.de>

--- brainfuck.f90
+++ brainfuck.f90
@@ -1,11 +1,10 @@
 program brainfuck
+  use iso_fortran_env, only: iostat_end
   use mod_brainfuck
   implicit none
 
   integer, parameter :: handle = 42
   character(len=*), parameter :: filename = 'hello.bf'
-  character(len=*), parameter :: program = "++++++++++[>+++++++>++++++++++>+++&
-&>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."
   character, dimension(:), allocatable :: program_array
   integer :: i, length, state
   character :: dummy
@@ -17,13 +16,8 @@ program brainfuck
      read(unit=handle, fmt='(A)', iostat=state, advance='no') dummy
      length = length + 1
      if(state /= 0) then
-        write(*, *) length, dummy, state
-        if(state == -2) then
-           read(unit=handle, fmt='(/)', iostat=state, advance='no')
-        elseif (state == -1) then
+        if (state == iostat_end) then
            exit
-        else
-           write(*, *) '>>> ', state
         end if
      end if
   end do
@@ -33,15 +27,13 @@ program brainfuck
 
   state = 0
 
-  open(unit=handle, file=filename, iostat=state)  
+  open(unit=handle, file=filename, iostat=state)
   do i = 1, length - 1
         read(unit=handle, fmt='(A)', iostat=state, advance='no') program_array(i)
-        if(state < 0) write(*, *) 'unexpected EOF at ', i
-
-        write(*, *) '>>> ', program_array(i), ' <<<'
+        if(state == iostat_end) write(*, *) 'unexpected EOF at ', i
   end do
   close(unit=handle)
-     
+
   call init(data_size = 30000, program_array = program_array)
 
   call run
